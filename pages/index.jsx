c05868663386d0230b97327c87107512223e2ed3
jay <jay.chen@castlery.com>
2024-11-28 16:21:39 +0800

    adjust homepage UI

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,15 @@
 import {
-  Page,Layout,Card,
+  Page,
+  Layout,
+  Card,
   Link,
-  Text, VerticalStack,
+  Text,
+  VerticalStack,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation, Trans } from "react-i18next";
 
 import { trophyImage } from "../assets";
-
 import { ProductsCard } from "../components";
 
 export default function HomePage() {
@@ -18,40 +20,39 @@ export default function HomePage() {
       <VerticalStack gap="4">
         {/* 第一行 */}
         <Layout>
+          {/* 第一列：显示分类信息 */}
           <Layout.Section>
             <Card sectioned>
               <Text as="p" variant="bodyMd" alignment="center">
-                <Link url="https://www.hao123.com"  external>
-                  hao123
-                </Link>
-                />
+                Categories
               </Text>
             </Card>
           </Layout.Section>
+          {/* 其他列：展示链接 */}
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
@@ -60,37 +61,80 @@ export default function HomePage() {
 
         {/* 第二行 */}
         <Layout>
+          {/* 第一列：显示分类信息 */}
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Text as="p" variant="bodyMd" alignment="center">
+                Categories
+              </Text>
+            </Card>
+          </Layout.Section>
+          {/* 其他列：展示链接 */}
+          <Layout.Section>
+            <Card sectioned>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
+                hao123
+              </Link>
+            </Card>
+          </Layout.Section>
+        </Layout>
+
+        {/* 第三行 */}
+        <Layout>
+          {/* 第一列：显示分类信息 */}
+          <Layout.Section>
+            <Card sectioned>
+              <Text as="p" variant="bodyMd" alignment="center">
+                Categories
+              </Text>
+            </Card>
+          </Layout.Section>
+          {/* 其他列：展示链接 */}
+          <Layout.Section>
+            <Card sectioned>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
           </Layout.Section>
           <Layout.Section>
             <Card sectioned>
-              <Link url="https://www.hao123.com"  external>
+              <Link url="https://www.hao123.com" external>
+                hao123
+              </Link>
+            </Card>
+          </Layout.Section>
+          <Layout.Section>
+            <Card sectioned>
+              <Link url="https://www.hao123.com" external>
+                hao123
+              </Link>
+            </Card>
+          </Layout.Section>
+          <Layout.Section>
+            <Card sectioned>
+              <Link url="https://www.hao123.com" external>
                 hao123
               </Link>
             </Card>
